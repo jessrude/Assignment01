0c5f44b811aa8ecdd5a29c217cd9f6db950f65d4
Jess Rude <jessicajoyrude@gmail.com>
2023-07-02 19:27:56 -0700

    testing

--- js/script.js
+++ js/script.js
@@ -6,4 +6,67 @@
 //let MyLibraryFunction
 
 //STEP 2
+
+//3.9 // numeric literal
+//'Hello!' // string literal
+//false // Boolean literal
+//null // literal null value
+
 //STEP 3
+
+
+// BASIC IF STATEMENT
+//let num = Number (prompt(`Enter a number between 1 and 10`))
+//if (num === 5) {
+//console.log(`You guessed the right number.`)
+//} 
+
+
+// BASIC IF/ELSE STATEMENT
+   // let num = Number(prompt(`Enter a number between 1 and 10`))
+   // if (num === 5) {
+   //     console.log (`You guessed the right number.`)
+   // } else { 
+   //     console.log(`You guessed the wrong number.`)
+  //  }
+
+// SINGLE STATEMENT SUITE
+//let num = Number(prompt(`Enter a number between 1 and 10`));
+//(num === 5) ? console.log(`Correct!`) : console.log(`Wrong!`);
+
+
+
+// IF ELSE/IF STATEMENT
+//alert(`Let\`s play rock, paper, scissors.`)
+//let choice = prompt(`Enter your choice: rock, paper, or scissors.`)
+// if (choice === 'rock') {
+//     console.log(`You chose rock!`)
+// } else if (choice === 'paper') {
+//     console.log(`You chose paper!`)
+// } else if (choice === `scissors`) { 
+//     console.log(`You chose scissors!`)
+// } else {
+//     console.log(`You didn\`t choose a valid option.`) 
+// }
+
+// SWITCH STATEMENT
+// alert(`Let\`s play rock, paper, scissors.`)
+// let choice = prompt(`Enter your choice: rock, paper, or scissors.`)
+// switch (choice) {
+//     case `rock`:
+//         console.log(`You chose rock!`)
+//         break
+//     case 'paper':
+//         console.log === (`You chose paper!`)
+//         break
+//     case 'scissors':
+//         console.log(`You chose scissors!`)
+//         break
+
+//     default: 
+//         console.log(`You didn\`t choose a valid option.`)
+
+// }
+
+
+
